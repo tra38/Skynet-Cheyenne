5336aa5afac18941a98fd14846e120c9aff30583
tra38 <tra38@nau.edu>
2015-08-04 12:57:55 -0500

    Spike attempt to generate 'inspiration' from noun

---
+++ generate_location.rb
@@ -0,0 +1,50 @@
+require 'faker'
+
+def normal_location_noun
+	['store', 'shop', 'superstore', 'megastore', 'boutique', 'emporium', 'market'].sample
+end
+
+def exotic_location_noun
+	['museum','plaza','center','foundation','academy'].sample
+end
+
+def generate_location(word)
+	string = word+" "+normal_location_noun
+	puts titleize(string)
+end
+
+def generate_think_tank(word)
+	puts titleize("The #{word} #{exotic_location_noun}")
+end
+
+def titleize(word)
+	word.split.map(&:capitalize).join(' ')
+end
+
+#Word.capitalize just capitalize only the first word, while this 'titieize' method will title each
+#individual word
+
+words_to_generate = ["hat", "car keys", "wedding", "sorrow", "violin case", "flower"]
+
+exotic_words = ['museum','plaza','center','foundation','academy']
+
+words_to_generate.each do |normal_word|
+	exotic_words.each do |exotic_word|
+		new_word = normal_word+" "+ exotic_word
+		puts titleize(new_word)
+	end
+end
+
+
+
+# words_to_generate.each do |word|
+# 	generate_location(word)
+# 	generate_think_tank(word)
+# end
+
+# Deleted Words: 'research facilitiy'
+
+# Store-related nouns:
+# 'store', 'shop', 'superstore', 'megastore', 'boutique', 'emporium', 'market'
+
+# Center-related nouns: 'plaza', 'museum', 'center'
