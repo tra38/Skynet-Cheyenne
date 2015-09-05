aa648108ce7978d14d48058ccbc6604141ee7138
tra38 <tra38@nau.edu>
2015-09-05 12:39:50 -0500

    Clean up test and add more code to ensure .any works even after returning all unique values

--- any.rb
+++ any.rb
@@ -1,10 +1,8 @@
 class Array
 	def any
-		p "starting possible choices: #{@possible_choices}" if @possible_choices
 		if (@possible_choices == nil || @possible_choices == [])
 			@possible_choices = self.dup
 		end
-		p "ending possible choices: #{@possible_choices}"
 		@possible_choices.shuffle!.shift
 	end
 end
--- spec/any_test.rb
+++ spec/any_test.rb
@@ -5,12 +5,12 @@ describe "#any" do
 		@array = [0,1,2,3,4,5]
 	end
 
-	it "returns a number from the array" do
+	it "returns an element from the array" do
 		number = @array.any
 		expect(@array.include?(number)).to be true
 	end
 
-	it "will not return duplicate numbers" do
+	it "will return unique elements from that array" do
 		output_array = []
 		6.times do
 			output_array << @array.any
@@ -19,6 +19,15 @@ describe "#any" do
 		expect(output_array).to eq (@array)
 	end
 
-	it ""
+	it "will still return elements from that array even after returning all unique elements" do
+		two_arrays = @array * 2
+		output_array = []
+		12.times do
+			output_array << @array.any
+		end
+		output_array.sort!
+		two_arrays.sort!
+		expect(output_array).to eq(two_arrays)
+	end
 
 end
